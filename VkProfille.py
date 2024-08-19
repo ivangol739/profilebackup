6e126439f8b8dbd24b82dc159a906083f64c3641
ivangol739 <ivangol739@gmail.com>
2024-08-19 22:47:20 +0300

    add methods photos and info

--- VkProfille.py
+++ VkProfille.py
@@ -1,10 +1,8 @@
 import requests
 import os
-import pprint
 
 
 class UserVk:
-
 	API_BASE_URL = 'https://api.vk.com/method'
 
 	def __init__(self, access_token, user_id):
@@ -15,6 +13,22 @@ class UserVk:
 		return {
 			"access_token": self.access_token,
 			"v": "5.131",
-			}
+		}
 
-	def
+	def _build_url(self, api_method):
+		return f"{self.API_BASE_URL}/{api_method}"
+
+	def user_info(self):
+		params = self.get_common_params()
+		params.update({"user_id": self.user_id})
+		response = requests.get(self._build_url("users.get"), params=params)
+		return response.json()
+
+	def get_photos_profile(self):
+		params = self.get_common_params()
+		params.update({"user_id": self.user_id,
+									 "album_id": "profile",
+									 "extented": "1",
+									 })
+		response = requests.get(self._build_url("photos.get"), params=params)
+		return response.json().
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from dotenv import load_dotenv
 import os
 from VkProfille import UserVk
 from YaUploader import YaUploader
-
-load_dotenv()
-
-api_token = os.getenv('API_TOKEN')
+from pprint import pprint
 
 if __name__ == "__main__":
-	a = UserVk()
-	b = YaUploader()
+	load_dotenv()
+	api_token = os.getenv('API_TOKEN_VK')
+	user_id = "90962492"
+	vk = UserVk(api_token, user_id)
+	pprint(vk.get_photos_profile())
