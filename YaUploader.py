63ec886bcc1d8dbca8dbc4eb08f2587b6eee82bd
ivangol739 <ivangol739@gmail.com>
2024-08-21 22:33:50 +0300

    add folder on Yandex Disk

--- YaUploader.py
+++ YaUploader.py
@@ -1,6 +1,29 @@
 import requests
 import os
+from pprint import pprint
+
+class YaUploader:
+	API_BASE_URL_YA = 'https://cloud-api.yandex.net/v1/disk/resources/'
+
+	def __init__(self, token):
+		self.token = token
+
+	def upload_photos(self):
+		directory = 'photos'
+
+		headers = {
+			'Authorization': self.token,
+			'Content-Type': 'application/json'
+		}
+		params = {
+			"path": directory,
+			"overwrite": "true"
+		}
+
+		requests.put(self.API_BASE_URL_YA, headers=headers, params=params)
+		current_dir = os.getcwd()
+		photos_dir = os.path.join(current_dir, directory)
+		photos = os.listdir(photos_dir)
+		print(photos)
 
 
-# class YaUploader:
-# 	print("check2")
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from dotenv import load_dotenv
 import os
 from VkProfille import UserVk
-# from YaUploader import YaUploader
+from YaUploader import YaUploader
 from pprint import pprint
 
 if __name__ == "__main__":
 	load_dotenv()
-	api_token = os.getenv('API_TOKEN_VK')
-	user_id = "90962492"
-	vk = UserVk(api_token, user_id)
-	pprint(vk.get_photos_profile())
+	# api_token = os.getenv('API_TOKEN_VK')
+	# user_id = "90962492"
+	# vk = UserVk(api_token, user_id)
+	# print(vk.get_photos_profile())
+	api_token_ya = os.getenv('API_TOKEN_YA')
+	ya = YaUploader(api_token_ya)
+	print(ya.upload_photos())
