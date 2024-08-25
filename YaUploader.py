2ba7429be594245cb58b312a825d38ea4472edc6
ivangol739 <ivangol739@gmail.com>
2024-08-25 21:18:18 +0300

    ready uploader class

--- YaUploader.py
+++ YaUploader.py
@@ -1,29 +1,44 @@
 import requests
 import os
 from pprint import pprint
+from tqdm import tqdm
+
 
 class YaUploader:
-	API_BASE_URL_YA = 'https://cloud-api.yandex.net/v1/disk/resources/'
+	API_BASE_URL_YA = "https://cloud-api.yandex.net/v1/disk/resources"
+	directory = "photos/"
 
 	def __init__(self, token):
 		self.token = token
 
-	def upload_photos(self):
-		directory = 'photos'
+	def get_common_params(self):
+		return {
+			"path": "",
+		}
 
-		headers = {
+	def get_common_headers(self):
+		return {
 			'Authorization': self.token,
 			'Content-Type': 'application/json'
 		}
-		params = {
-			"path": directory,
-			"overwrite": "true"
-		}
 
-		requests.put(self.API_BASE_URL_YA, headers=headers, params=params)
+	def create_folder(self):
+		params = self.get_common_params()
+		params.update({"path": self.directory})
+		requests.put(self.API_BASE_URL_YA, headers=self.get_common_headers(), params=params)
+
+	def upload_photos(self):
+		self.create_folder()
 		current_dir = os.getcwd()
-		photos_dir = os.path.join(current_dir, directory)
+		photos_dir = os.path.join(self.directory)
 		photos = os.listdir(photos_dir)
-		print(photos)
-
-
+		for photo in tqdm(photos):
+			params = self.get_common_params()
+			params.update({
+				"path": f"{photos_dir}{photo}",
+				"overwrite": "true"
+			})
+			url_for_upload = requests.get(f"{self.API_BASE_URL_YA}/upload", headers=self.get_common_headers(), params=params)
+			href = url_for_upload.json()["href"]
+			upload = requests.put(href, open(photos_dir + photo, "rb"))
+		return f"Файлы загружены"
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from pprint import pprint
 
 if __name__ == "__main__":
 	load_dotenv()
-	# api_token = os.getenv('API_TOKEN_VK')
-	# user_id = "90962492"
-	# vk = UserVk(api_token, user_id)
-	# print(vk.get_photos_profile())
+	api_token = os.getenv('API_TOKEN_VK')
+	user_id = "90962492"
+	vk = UserVk(api_token, user_id)
+	print(vk.get_photos_profile())
 	api_token_ya = os.getenv('API_TOKEN_YA')
 	ya = YaUploader(api_token_ya)
 	print(ya.upload_photos())
